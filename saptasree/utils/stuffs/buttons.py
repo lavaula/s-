b7076aa971aa8d1b3032727a3f1f8c2ba6678b5e
RiyoGod <godsxrio@gmail.com>
2025-04-06 14:06:39 +0530

    Update buttons.py

--- saptasree/utils/stuffs/buttons.py
+++ saptasree/utils/stuffs/buttons.py
@@ -2,24 +2,17 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram import Client, filters, enums 
 
 class BUTTONS(object):
-    MBUTTON = [[InlineKeyboardButton("◈ ɢʀᴏᴜᴘ ◈", callback_data="mplus HELP_Group"), 
-    InlineKeyboardButton("◈ sᴛɪᴄᴋᴇʀs ◈", callback_data="mplus HELP_Sticker"),
-    InlineKeyboardButton("◈ ᴀɪ-ʜᴇʟᴘ ◈", callback_data="mplus HELP_ChatGPT")],
-    [InlineKeyboardButton("◈ ɪɴꜰᴏ ◈", callback_data="mplus HELP_Info"), 
-    InlineKeyboardButton("◈ ᴛᴀɢ-ᴀʟʟ ◈", callback_data="mplus HELP_TagAll"),
-    InlineKeyboardButton("◈ ᴇxᴛʀᴀs ◈", callback_data="mplus HELP_Extra")],
-    [InlineKeyboardButton("◈ ʙᴀɴ-ᴜɴʙᴀɴ ◈", callback_data="mplus HELP_Action"),
-    InlineKeyboardButton("◈ ɢᴏᴏɢʟᴇ ◈", callback_data="mplus HELP_Search"),
-    InlineKeyboardButton("◈ ɪᴍᴀɢᴇ ◈", callback_data="mplus HELP_Image")],    
-    [InlineKeyboardButton("◈ ᴛɢᴍ ◈", callback_data="mplus HELP_TG"),
-    InlineKeyboardButton("◈ ɢᴀᴍᴇs ◈", callback_data="mplus HELP_Game"),
-    InlineKeyboardButton("◈ ғᴏɴᴛ ◈", callback_data="mplus HELP_Font")],
-    [InlineKeyboardButton("◈ ᴛʀᴜᴛʜ-ᴅᴀʀᴇ ◈", callback_data="mplus HELP_TD"),
-    InlineKeyboardButton("◈ ʜᴀsᴛᴀɢ ◈", callback_data="mplus HELP_HT"),
-    InlineKeyboardButton("◈ ɪᴍᴘᴏsᴛᴇʀ ◈", callback_data="mplus HELP_Imposter")], 
-    [InlineKeyboardButton("◈ ǫᴜᴏᴛʟʏ ◈", callback_data="mplus HELP_Q"),
-    InlineKeyboardButton("◈ ғᴜɴ ◈", callback_data="mplus HELP_Fun"),
-    InlineKeyboardButton("◈ ᴛᴛs ◈", callback_data="mplus HELP_TTS")],          
-    [InlineKeyboardButton("", callback_data=f"managebot123 settings_back_helper"), 
-    InlineKeyboardButton("< ʀᴇᴛᴜʀɴ ", callback_data=f"settings_back_helper")]
-]
+    MBUTTON = [[InlineKeyboardButton("CʜᴀᴛGPT", callback_data="mplus HELP_ChatGPT"),InlineKeyboardButton("ɢʀᴏᴜᴘs", callback_data="mplus HELP_Group"),InlineKeyboardButton("sᴛɪᴄᴋᴇʀs", callback_data="mplus HELP_Sticker")],
+    [InlineKeyboardButton("Tᴀɢ-Aʟʟ", callback_data="mplus HELP_TagAll"),
+    InlineKeyboardButton("Iɴꜰᴏ", callback_data="mplus HELP_Info"),InlineKeyboardButton("Exᴛʀᴀ", callback_data="mplus HELP_Extra")],
+    [InlineKeyboardButton("Iᴍᴀɢᴇ", callback_data="mplus HELP_Image"),
+    InlineKeyboardButton("Aᴄᴛɪᴏɴ", callback_data="mplus HELP_Action"),InlineKeyboardButton("Sᴇᴀʀᴄʜ", callback_data="mplus HELP_Search")],    
+    [InlineKeyboardButton("ғᴏɴᴛ", callback_data="mplus HELP_Font"),
+    InlineKeyboardButton("ɢᴀᴍᴇs", callback_data="mplus HELP_Game"),InlineKeyboardButton("Ⓣ-ɢʀᴀᴘʜ", callback_data="mplus HELP_TG")],
+    [InlineKeyboardButton("ɪᴍᴘᴏsᴛᴇʀ", callback_data="mplus HELP_Imposter"),
+    InlineKeyboardButton("Tʀᴜᴛʜ-ᗪᴀʀᴇ", callback_data="mplus HELP_TD"),InlineKeyboardButton("ʜᴀsᴛᴀɢ", callback_data="mplus HELP_HT")], 
+    [InlineKeyboardButton("ᴛᴛs", callback_data="mplus HELP_TTS"),
+    InlineKeyboardButton("ғᴜɴ", callback_data="mplus HELP_Fun"),InlineKeyboardButton("ǫᴜᴏᴛʟʏ", callback_data="mplus HELP_Q")],          
+    [InlineKeyboardButton("<", callback_data=f"settings_back_helper"), 
+    InlineKeyboardButton(">", callback_data=f"managebot123 settings_back_helper"),
+    ]]

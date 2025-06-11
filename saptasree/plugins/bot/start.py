d953dce2279faf1331a45ccd831173fcba4be2d8
𝐏 𝐄 𝐍 𝐓 𝐄 𝐗 𝐘 <godsxrio@gmail.com>
2025-06-12 01:37:01 +0530

    Update start.py

--- saptasree/plugins/bot/start.py
+++ saptasree/plugins/bot/start.py
@@ -30,7 +30,7 @@ from strings import get_string
 
 NEXI_VID = [
 "https://files.catbox.moe/eafgjj.mp4" ,
-"https://files.catbox.moe/eafgjj.mp4" ,
+"https://files.catbox.moe/d4unti.mp4" ,
 "https://files.catbox.moe/eafgjj.mp4" ,
 ]
 

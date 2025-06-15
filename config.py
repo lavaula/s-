93f2f7cfb77408fe4d8ec779085b80161912a952
𝐏 𝐄 𝐍 𝐓 𝐄 𝐗 𝐘 <godsxrio@gmail.com>
2025-06-15 20:07:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID"))
 
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", "NxGBNexGenBotsd22d66") 
+API_KEY = getenv("API_KEY", "30DxNexGenBotsa0f22f") 
 
 API_HASH = getenv("API_HASH")
 

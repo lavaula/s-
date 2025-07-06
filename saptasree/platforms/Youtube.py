563017a325c478a083bc00b006d81fc3fd44edd5
𝐏 𝐄 𝐍 𝐓 𝐄 𝐗 𝐘 <godsxrio@gmail.com>
2025-07-06 23:29:24 +0530

    Update Youtube.py

--- saptasree/platforms/Youtube.py
+++ saptasree/platforms/Youtube.py
@@ -9,8 +9,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from ANNIEMUSIC.utils.database import is_on_off
-from ANNIEMUSIC.utils.formatters import time_to_seconds
+from saptasree.utils.database import is_on_off
+from saptasree.utils.formatters import time_to_seconds
 
 
 
